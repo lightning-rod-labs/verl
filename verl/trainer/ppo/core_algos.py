4d9fe7586b57579f7077d661f79cc82cd4faa8e8
Danny Franklin <danny@dannyfranklin.com>
2025-04-02 14:52:57 -0700

    Fix and remove std computation

--- verl/trainer/ppo/core_algos.py
+++ verl/trainer/ppo/core_algos.py
@@ -131,6 +131,9 @@ def compute_grpo_outcome_advantage(token_level_rewards: torch.Tensor,
     response_length = token_level_rewards.shape[-1]
     scores = token_level_rewards.sum(dim=-1)
 
+    id2score = defaultdict(list)
+    id2mean = {}
+
     with torch.no_grad():
         if mean_subtraction:
             print("Computing GRPO with mean averaging")
@@ -140,10 +143,8 @@ def compute_grpo_outcome_advantage(token_level_rewards: torch.Tensor,
             for idx in id2score:
                 if len(id2score[idx]) == 1:
                     id2mean[idx] = torch.tensor(0.0)
-                    id2std[idx] = torch.tensor(1.0)
                 elif len(id2score[idx]) > 1:
                     id2mean[idx] = torch.mean(torch.tensor(id2score[idx]))
-                    id2std[idx] = torch.std(torch.tensor([id2score[idx]]))
                 else:
                     raise ValueError(f"no score in prompt index: {idx}")
             for i in range(bsz):
