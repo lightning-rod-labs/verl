e7f7f061bdfc389ca63b324e21dcf0cb15a95a6e
Danny Franklin <danny@dannyfranklin.com>
2025-04-02 14:33:21 -0700

    Add in mean subtraction to grpo

--- verl/trainer/ppo/core_algos.py
+++ verl/trainer/ppo/core_algos.py
@@ -131,8 +131,21 @@ def compute_grpo_outcome_advantage(token_level_rewards: torch.Tensor,
     scores = token_level_rewards.sum(dim=-1)
 
     with torch.no_grad():
-        print("Computing GRPO without normalization")
-        # Note: We removed normalization of the rewards that normally happens in GRPO
+        print("Computing GRPO with mean averaging")
+        bsz = scores.shape[0]
+        for i in range(bsz):
+            id2score[index[i]].append(scores[i])
+        for idx in id2score:
+            if len(id2score[idx]) == 1:
+                id2mean[idx] = torch.tensor(0.0)
+                id2std[idx] = torch.tensor(1.0)
+            elif len(id2score[idx]) > 1:
+                id2mean[idx] = torch.mean(torch.tensor(id2score[idx]))
+                id2std[idx] = torch.std(torch.tensor([id2score[idx]]))
+            else:
+                raise ValueError(f"no score in prompt index: {idx}")
+        for i in range(bsz):
+            scores[i] = (scores[i] - id2mean[index[i]])
         scores = scores.unsqueeze(-1).tile([1, response_length]) * eos_mask
 
     return scores, scores
